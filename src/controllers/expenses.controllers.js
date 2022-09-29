387dc346e8475f1d3896b2c253f5f998450e1036
Sergi <serginavarrapares@gmail.com>
2022-09-29 18:47:37 +0200

    Adding expenses to the database

--- src/controllers/expenses.controllers.js
+++ src/controllers/expenses.controllers.js
@@ -1,3 +1,4 @@
+const pool = require('./../database');
 
 const getExpenses = async (req, res) => {
     const user = await req.user;
@@ -9,10 +10,19 @@ const getAdd = async (req, res) => {
     res.render('add', { username: user.username });
 }
 
+const postAdd = async (req, res) => {
+    const { category, comment, amount } = req.body;
+    const user = await req.user;
+    const newDate = new Date().toDateString();
+    await pool.query('INSERT INTO expenses (u_id, date, category, comment, amount) VALUES ($1, $2, $3, $4, $5)', 
+        [user.u_id, newDate, category, comment, amount]);
+    res.redirect('/expenses');
+}
+
 const getExpenseList = async (req, res) => {
     const { name } = req.params;
     const user = await req.user;
     res.render('list', { name: name, username: user.username });
 }
 
-module.exports = { getExpenses, getExpenseList, getAdd };
+module.exports = { getExpenses, getExpenseList, getAdd, postAdd };
--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { getExpenses, getExpenseList, getAdd } = require('./../controllers/expenses.controllers.js');
+const { getExpenses, getExpenseList, getAdd, postAdd } = require('./../controllers/expenses.controllers.js');
 const router = Router();
 
 router.get('/expenses', getExpenses);
 
 router.get('/expenses/add', getAdd);
+
+router.post('/expenses/add', postAdd);
 
 router.get('/expenses/:name', getExpenseList);
 
